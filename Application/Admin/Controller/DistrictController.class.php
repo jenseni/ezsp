5cec0278c942f46e58015f4df2de7ada7c9249c9
P2Ruthless <wanghe3707@163.com>
2014-06-13 19:53:57 +0800

    区域数据添加 数据文件与其他分开

--- Application/Admin/Controller/DistrictController.class.php
+++ Application/Admin/Controller/DistrictController.class.php
@@ -1,9 +1,7 @@
 <?php
 namespace Admin\Controller;
 
-use \Think\Controller;
-
-class DistrictController extends Controller{
+class DistrictController extends AdminController{
 	public function getchildren(){
 		$pid = I('get.city', I('get.area'));
 
@@ -28,5 +26,40 @@ class DistrictController extends Controller{
 		}
 
 		$this->ajaxReturn($ret);
+	}
+
+	public function lists($pid = 1){
+		$this->authView(121);
+
+		$District = M('District');
+		$parentDistrict = $District->field(true)->find($pid);
+
+		if(empty($parentDistrict)){
+			$this->error('区域不存在或已删除');
+		}
+		//get path from root
+		$rootPath = array($parentDistrict);
+		$curParent = $parentDistrict;
+		while($curParent['id'] != $curParent['pid']){
+			$curParent = $District->field(true)->where(array('id'=>$curParent['pid']))->find();
+			if(!empty($curParent)){
+				array_unshift($rootPath, $curParent);
+			}else{
+				break;
+			}
+		}
+
+		$parentDistrict['rootPath'] = $rootPath;
+
+		// fetch children
+		$districtList = $District->field(true)
+			->where("pid=%d AND pid<>id", $pid)
+			->select();
+
+		$parentDistrict['children'] = $districtList;
+
+		$this->assign('parentDistrict', $parentDistrict);
+
+		$this->display();
 	}
 }
