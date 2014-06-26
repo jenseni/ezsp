2a0365b726145aa839ae1f1254f401b706051921
jenseni <kkltzjgnv@163.com>
2014-06-26 22:10:05 +0800

    phone，房屋买卖，写字楼商铺

--- Application/Phone/Controller/HouseSaleController.class.php
+++ Application/Phone/Controller/HouseSaleController.class.php
@@ -2,15 +2,54 @@
 namespace Phone\Controller;
 
 class HouseSaleController extends PhoneController{
-	public function index(){
+	/*public function index(){
+		$HouseSale = M('Housesale');
+		$search = I('get.search');
+		$wheresql = 'h.status = 1';
+
+		if(!empty($search)){
+			$wheresql = $wheresql.' and (h.community like \'%'.$search.'%\' or h.title like \'%'.$search.'%\')';
+		}
+		
+		$totalCount = $HouseSale->alias('h')
+			->where($wheresql)
+			->count(1);
+
+		$page = new \Think\Page($totalCount);
+		$page->rollPage = 3;
+		$page->setConfig('prev', '上一页');
+		$page->setConfig('next', '下一页');
+		$page->setConfig('theme', '%UP_PAGE% %LINK_PAGE% %DOWN_PAGE%');
+
+		$dataList = $HouseSale->alias('h')
+			->field('h.id,h.bed_room,h.live_room,h.decorate,h.price,h.thumbnail,h.community,busi_area.name busi_area_name,h.title,h.square')
+			->join('__DISTRICT__ busi_area on busi_area.id=h.busi_area', 'LEFT')
+			//->where(array('h.status'=>array('NEQ', 0)))
+			->where($wheresql)
+			->order('level desc, id desc')
+			->limit($page->firstRow, $page->listRows)
+			->select();
+		if(!empty($dataList)){
+			$this->assign('dataList', $dataList);
+		}
+		$this->assign('page',$page->show());
 		$this->display();
-	}
+	}*/
 
 	public function lists($area = '0', $price = '0', $square = '0', $room = '0', $sort = ''){
+		
 		$map = array();
 
 		$map['h.city'] = (int)$this->city['id'];
 		$map['h.status'] = 1;
+
+		$search = I('get.search');
+		if(!empty($search)){
+			$where['h.title']  = array('like', '%'.$search.'%');
+			$where['h.community'] = array('like','%'.$search.'%');
+			$where['_logic'] = 'or';
+			$map['_complex'] = $where;
+		}
 
 		if($area != '0'){
 			$map['_string'] = "h.area=$area OR h.busi_area=$area";
@@ -77,6 +116,8 @@ class HouseSaleController extends PhoneController{
 		$this->assign('square', $square);
 		$this->assign('room', $room);
 		$this->assign('page', $page->show());
+
+		$this->assign('nav',1);
 
 		$this->display();
 	}
